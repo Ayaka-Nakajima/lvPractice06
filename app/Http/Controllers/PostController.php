841ac4a529471ed9de90e87236f05476185d46a2
Ayaka-Nakajima <ayakanakajima20000918@gmail.com>
2024-06-16 19:48:40 +0900

    ブログ投稿一覧の表示

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,9 @@ class PostController extends Controller
     //インポートしたPostをインスタンス化して$postとして使用。
     public function index(Post $post)
     {
-        return $post->get();//$postの中身を戻り値にする。
+        //return $post->get();//$postの中身を戻り値にする。
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+        //getPaginateByLimit()はPost.phpで定義したメソッドです。
     }
 }
 //use宣言は外部にあるクラスをPostController内にインポートできる。
